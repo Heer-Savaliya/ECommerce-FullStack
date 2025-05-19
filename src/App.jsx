18be641822f516a156f4d2b292205b2f601f86a9
Heer Savaliya <heersavaliya65@gmail.com>
2025-05-19 10:36:29 +0530

    folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import AppLayout from "./components/Layout/AppLayout";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Blog from "./Pages/Blog";
+import MyAccount from "./Pages/MyAccount";
+import ForgetPassword from "./Pages/ForgetPassword";
+import PageNotFound from "./Pages/PageNotFound";
 
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const router = createBrowserRouter([
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path:"/",
+      element:<AppLayout />,
+      children:[
+        {
+          path:"/",
+          element:<Home />
+        },
+        {
+          path:"/about",
+          element:<About />
+        },
+        {
+          path:"/contact",
+          element:<Contact />
+        },
+        {
+          path:"/blog",
+          element:<Blog />
+        },
+        {
+          path:"/my-account",
+          element:<MyAccount />
+        },
+        {
+          path:"/forget-pass",
+          element:<ForgetPassword />
+        },
+        {
+          path:"*",
+          element:<PageNotFound />
+        },
+      ]
+    },
+    {
+      path:"*",
+      element:<PageNotFound />
+    }
+  ]);
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
