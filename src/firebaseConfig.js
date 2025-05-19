75634b74415bfe6c3b5179a75c3471faada50b03
Heer Savaliya <heersavaliya65@gmail.com>
2025-05-19 12:36:34 +0530

    firebase config

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDIBxClDVfzkfG3_irCXOl26buG02-OMOI",
+  authDomain: "e-com-grocery.firebaseapp.com",
+  projectId: "e-com-grocery",
+  storageBucket: "e-com-grocery.firebasestorage.app",
+  messagingSenderId: "697862994897",
+  appId: "1:697862994897:web:396158f8861dfd57eafef0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
