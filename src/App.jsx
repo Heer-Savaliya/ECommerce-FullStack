4afacdd8ee7045888d29bba956df597b646c100d
Heer Savaliya <heersavaliya65@gmail.com>
2025-05-21 14:21:59 +0530

    private routing

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Blog from "./Pages/Blog";
 import MyAccount from "./Pages/MyAccount";
 import ForgetPassword from "./Pages/ForgetPassword";
 import PageNotFound from "./Pages/PageNotFound";
+import PrivateRoute from "./Routes/PrivateRoute";
 
 const App = () => {
   const router = createBrowserRouter([
@@ -22,43 +23,71 @@ const App = () => {
       element: <Register />,
     },
     {
-      path:"/",
-      element:<AppLayout />,
-      children:[
+      path: "/",
+      element: <AppLayout />,
+      children: [
         {
-          path:"/",
-          element:<Home />
+          path: "/",
+          element: (
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          ),
         },
         {
-          path:"/about",
-          element:<About />
+          path: "/about",
+          element: (
+            <PrivateRoute>
+              <About />
+            </PrivateRoute>
+          ),
         },
         {
-          path:"/contact",
-          element:<Contact />
+          path: "/contact",
+          element: (
+            <PrivateRoute>
+              <Contact />
+            </PrivateRoute>
+          ),
         },
         {
-          path:"/blog",
-          element:<Blog />
+          path: "/blog",
+          element: (
+            <PrivateRoute>
+              <Blog />
+            </PrivateRoute>
+          ),
         },
         {
-          path:"/my-account",
-          element:<MyAccount />
+          path: "/my-account",
+          element: (
+            <PrivateRoute>
+              <MyAccount />
+            </PrivateRoute>
+          ),
         },
         {
-          path:"/forget-pass",
-          element:<ForgetPassword />
+          path: "/forget-pass",
+          element: (
+            <PrivateRoute>
+              <ForgetPassword />
+            </PrivateRoute>
+          ),
         },
         {
-          path:"*",
-          element:<PageNotFound />
+          path: "*",
+          element: (
+            <PrivateRoute>
+              <PageNotFound />
+            </PrivateRoute>
+          ),
         },
-      ]
+      ],
     },
     {
-      path:"*",
-      element:<PageNotFound />
-    }
+      path: "*",
+      element: <PageNotFound />,
+    },
   ]);
   return <RouterProvider router={router} />;
 };
---
+++ src/Routes/PrivateRoute.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { auth } from '../firebaseConfig'
+import { useAuthState } from "react-firebase-hooks/auth";
+import { Navigate } from 'react-router-dom';
+
+const PrivateRoute = ({children}) => {
+    const [user,loading] = useAuthState(auth);
+    if(loading){
+        return <p>Loading</p>;
+    }
+    return user ? children : <Navigate to="/login"/>;
+  
+}
+
+export default PrivateRoute
