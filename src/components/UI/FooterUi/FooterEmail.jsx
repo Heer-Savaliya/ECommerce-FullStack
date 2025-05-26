40417c70956ff9c5d45ea2183a6391e8dcf673aa
Heer Savaliya <heersavaliya65@gmail.com>
2025-05-26 15:56:16 +0530

    Updated footer ui

--- src/components/UI/FooterUi/FooterEmail.jsx
+++ src/components/UI/FooterUi/FooterEmail.jsx
@@ -12,8 +12,8 @@ const FooterEmail = () => {
           worry , we not spam !
         </p>
       </div>
-      <div className="w-full md:w-auto">
-        <div className="bg-white flex border-2 border-gray-400 rounded-[10px]  gap-2">
+      <div className="w-full md:w-auto ">
+        <div className="bg-white flex border-2 border-gray-300 rounded-[10px]  gap-2">
           <div className="p-2 lg:p-2.5 flex flex-1 items-center gap-2">
             <img
               src="./images/message_logo.png"
@@ -30,7 +30,7 @@ const FooterEmail = () => {
           </div>
           <div className="bg-purple h-auto p-2 lg:p-2.5 px-5 lg:px-6 rounded-r-[8px]">
 
-          <button className="capitalize font-semibold text-white bg-purple text-xs lg:text-base">send</button>
+          <button className="capitalize font-semibold text-white bg-purple text-xs lg:text-base border-none outline-none">send</button>
           </div>
         </div>
         <h1 className="text-xs mt-2 text-gray-500 hidden lg:block">By subscribing you agree to our <span className="text-purple">Terms & Conditions and Privacy & Cookies Policy</span></h1>
